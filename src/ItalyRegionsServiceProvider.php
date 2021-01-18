f91f878e0182872632a186194be2d5bc24cf16c2
lfbellante <lfbellante@gmail.com>
2021-01-18 22:56:57 +0100

    chore: created version 0.1.0

---
+++ src/ItalyRegionsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace Lfbellante\ItalyRegions;
+use Illuminate\Support\ServiceProvider;
+
+class ItalyRegionsServiceProvider extends ServiceProvider
+{
+    /**
+    * Publishes configuration file.
+    *
+    * @return  void
+    */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/regions.php' => config_path('regions.php'),
+        ], 'regions-config');
+    }
+    /**
+    * Make config publishment optional by merging the config from the package.
+    *
+    * @return  void
+    */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/regions.php',
+            'regions'
+        );
+    }
+}
---
+++ src/config/regions.php
@@ -0,0 +1,5 @@
+<?php
+    return [
+        'source_type' => 'json',
+        'source' => "https://raw.githubusercontent.com/matteocontrini/comuni-json/master/comuni.json"
+    ];
